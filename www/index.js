28315fa2137f2474e0926ea3b7d1e46772626212
artkon07r <artkon07r@gmail.com>
2025-11-12 10:19:49 +0500

    Add files via upload

---
+++ www/index.js
@@ -0,0 +1,8 @@
+let theme=document.querySelector(".like_1");
+
+theme.addEventListener("click",click_like1);
+
+function click_like1(){
+    document.querySelector(".like1").classList.toggle("like_btn1");
+    like_1.classList.toggle("like_btn1");
+}
