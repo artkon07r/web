699501433fb51505de8f171d60f1ed8cda686d9d
artkon07r <artkon07r@gmail.com>
2025-11-12 10:39:10 +0500

    Delete www directory

--- www/index.js
+++
@@ -1,8 +0,0 @@
-let theme=document.querySelector(".like_1");
-
-theme.addEventListener("click",click_like1);
-
-function click_like1(){
-    document.querySelector(".like1").classList.toggle("like_btn1");
-    like_1.classList.toggle("like_btn1");
-}
